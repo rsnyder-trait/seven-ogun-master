6fb916215f2f771d0c337bf0af79233002602e55
rsnyder <rsnyder@traitstudio.com>
2014-05-05 12:18:18 -0400

    remove clients and partners, remove all service except coating and container, remove antonio Muchado

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,23 +2,23 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :home, 'Home', '/'
     primary.item :about, 'About', '/about', :highlights_on => /\/about/
-    primary.item :solutions, 'Solutions', '/solutions/card-solutions', :highlights_on => /\/solutions/, :class => 'subnav' do |solutions|
-      solutions.item :card_solutions, 'Card Solutions', '/solutions/card-solutions'
+    primary.item :solutions, 'Solutions', '/solutions/coating-solutions', :highlights_on => /\/solutions/, :class => 'subnav' do |solutions|
+#      solutions.item :card_solutions, 'Card Solutions', '/solutions/card-solutions'
       solutions.item :coating_solutions, 'Coating Solutions', '/solutions/coating-solutions'
       solutions.item :container_solutions, 'Container Solutions', '/solutions/container-solutions'
-      solutions.item :equipment, 'Equipment', '/solutions/equipment'
-      solutions.item :inkjet_solutions, 'Inkjet Solutions', '/solutions/inkjet-solutions'
-      solutions.item :rfid, 'RFID', '/solutions/rfid'
+#      solutions.item :equipment, 'Equipment', '/solutions/equipment'
+#      solutions.item :inkjet_solutions, 'Inkjet Solutions', '/solutions/inkjet-solutions'
+#      solutions.item :rfid, 'RFID', '/solutions/rfid'
     end
-    primary.item :clients, 'Clients &amp; Partners', '/clients-and-partners/wfb', :highlights_on => /\/clients-and-partners/, :class => 'subnav' do |clients|
-      clients.item :ashworth, 'Ashworth', '/clients-and-partners/ashworth'
-      clients.item :graphtech, 'Graphtech', '/clients-and-partners/graphtech'
-      clients.item :heights, 'Heights', '/clients-and-partners/heights'
-      clients.item :wfb, 'PTS', '/clients-and-partners/pts'
-      clients.item :real_smart, 'RealSmart', '/clients-and-partners/real_smart'
-      clients.item :trelleborg, 'Trelleborg', '/clients-and-partners/trelleborg'
-      clients.item :wfb, 'WFB', '/clients-and-partners/wfb'
-    end
+#    primary.item :clients, 'Clients &amp; Partners', '/clients-and-partners/wfb', :highlights_on => /\/clients-and-partners/, :class => 'subnav' do |clients|
+#      clients.item :ashworth, 'Ashworth', '/clients-and-partners/ashworth'
+#      clients.item :graphtech, 'Graphtech', '/clients-and-partners/graphtech'
+#      clients.item :heights, 'Heights', '/clients-and-partners/heights'
+#      clients.item :wfb, 'PTS', '/clients-and-partners/pts'
+#     clients.item :real_smart, 'RealSmart', '/clients-and-partners/real_smart'
+#      clients.item :trelleborg, 'Trelleborg', '/clients-and-partners/trelleborg'
+#      clients.item :wfb, 'WFB', '/clients-and-partners/wfb'
+#    end
     primary.item :contact, 'Contact', '/contact'
   end
 end
